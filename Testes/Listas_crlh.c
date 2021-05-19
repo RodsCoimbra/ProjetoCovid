d544548041b472546468da0221a9b5805d27faa8
Andre Teodosio <andre.teodosio@tecnico.ulisboa.pt>
2021-05-19 12:30:01 +0100

    função criar pais e detalhes inacabada

--- Testes/Listas_crlh.c
+++ Testes/Listas_crlh.c
@@ -7,44 +7,30 @@
 
 typedef struct detalhes { // lista que está dentro da lista "pais"
     int n_dorc; // numero de deaths or cases
-    int popu;
     int week_count;
     int year_week;
     float lastfteen;
-    char cont[8];
-    char pais[35];
-    char cod_pais[4];
     char indic[7];
-    struct detalhes *next;
+    struct detalhes *nextD; // nextD é o pointer do detalhe seguinte
 } detalhes;
 
-typedef struct pais {   // pais é a nova "linha"
-    struct detalhes *start;
-    struct pais *pais_next;
+typedef struct pais {
+    char pais[35];
+    char cod_pais[4];
+    char cont[8];
+    int popu;
+    detalhes *nextD;
+    struct pais *nextP; // nextP é o pointer do pais seguinte
 } pais;
 
-void inserir_detalhes (struct detalhes **head, int n_dorc, int popu, int week_count, int year_week, float lastfteen, char cont, char pais, char cod_pais, char indic){
+pais *inserir_paises (struct detalhes **head, int popu, char cont, char pais, char cod_pais){
     struct detalhes, *temp, *curr;
-    temp = malloc(sizeof(struct detalhes));
-    temp->n_dorc = n_dorc;
-    //inserir o resto do dados//
-    temp->next = NULL;
-
-    if (*head == NULL) {
-        *head = temp;
-    }
-    else{
-        current =(*head);
-        while(current->next!=NULL){
-            current=current->next;
-        }
-        current->next=temp;
-    }
+    
 }
 
 //  insert lists of integers in a list
 
-void inserir_paises (struct pais **pais_list,struct detalhes *head){
+detalhes *inserir_detalhes (struct pais **pais_list,struct detalhes *head){
     struct list *temp,*current;
     temp=malloc(sizeof(struct list));
     temp->start=head;
@@ -59,44 +45,6 @@ void inserir_paises (struct pais **pais_list,struct detalhes *head){
         }
         current->listnext=temp;
     }
-}
-
-// Show all the list with their data
-
-void show(struct list *pais_list){
-    int i=1;
-    struct pais *current;
-    struct detalhes *currentlist;
-    current=pais_list;
-    while(current!=NULL){
-        currentlist=current->start;
-        printf("List %d: ",i);
-        while(currentlist!=NULL){
-            printf("%d ",currentlist->_);
-            currentlist=currentlist->next;
-        }
-        i++;
-        printf("\n");
-        current=current->listnext;
-    }
-}
-
-int main(){
-    struct node *head1=NULL,*head2=NULL,*head3=NULL; // 3 lists of integers
-    struct list *pais_list=NULL;  // pais_list will be the head of lists of list
-    insertNode(&head1,20);  // inserting in first list
-    insertNode(&head1,13);
-    insertNode(&head1,22);
-    insertNode(&head1,18);
-    insertNode(&head2,42);  // inserting in second list
-    insertNode(&head2,15);
-    insertNode(&head3,12);  // inserting in third list
-    insertNode(&head3,14);
-    insertNode(&head3,28);
-    insertList(&pais_list,head1); // inserting lists in list
-    insertList(&pais_list,head2);
-    insertList(&pais_list,head3);
-    show(pais_list);
 }
 
 /*
